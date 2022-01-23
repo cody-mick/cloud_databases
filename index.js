ab07f357a3b08a95a900b077c432dac6aa40e3a5
cody-mick <cmick@byui.edu>
2022-01-22 17:30:14 -0700

    ._.

---
+++ index.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB6S9JeKOaYiWiuBpLWwDUTLn9S2uz2BBs",
+  authDomain: "grower-codes.firebaseapp.com",
+  projectId: "grower-codes",
+  storageBucket: "grower-codes.appspot.com",
+  messagingSenderId: "669430447435",
+  appId: "1:669430447435:web:0a7e9cb1af9d1a4a40fcce",
+  measurementId: "G-VRFNH1Z5QY",
+};
+
+const app = initializeApp(firebaseConfig);
